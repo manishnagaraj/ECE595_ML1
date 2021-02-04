3deb1af1eeb63f6b7f2a84714493d8b35c71f88a
manishnagaraj <manishnagaraj1@gmail.com>
2021-02-04 13:29:51 -0500

    2 completed

--- Homework1/Homework1-Exercise2.py
+++ Homework1/Homework1-Exercise2.py
@@ -15,17 +15,13 @@ import numpy.matlib
 import matplotlib.pyplot as plt
 import scipy.stats as stats
 from scipy.linalg import fractional_matrix_power
-
+import math
 # (a) 
-x1 = np.linspace(-1, 5, 1000)
-x2 = np.linspace(0, 10, 1000)
-y = np.zeros([1000, 1000])
-
-for i in range(1000):
-    for j in range(1000):
-        # eqn = 2x1**2 + 2x2**2 - 2*x1*x2 +4*x1 -20*x2 +56
-        eqn = (2*(x1[i]**2)) + (2*(x2[j]**2)) - (2*x1[i]*x2[j]) + (4*x1[i]) - (20*x2[j]) + 56
-        y[i][j] = (1/np.sqrt(12*(np.pi**2)))*np.exp((-1/6)*eqn)
+step = 0.01
+x1, x2 = np.mgrid[-1:5+step:step, 0:10+step:step]
+num = (2*(x1**2)) + (2*(x2**2)) + (-2*x1*x2) + (4*x1) - (20*x2) + 56
+den = math.sqrt(4 * math.pi * math.pi * 3)
+y = np.exp(-num / 6) / den
 plt.figure(figsize=(12,8))
 plt.contour(x1,x2,y)
 plt.xlabel('X1', fontsize=24)
@@ -38,9 +34,9 @@ plt.show()
 # (c)
 #(i) Use numpy.random.multivariate_normal to draw 5000 random samples from the 2D standard
 # normal distribution, and make a scatter plot of the data point using matplotlib.pyplot.scatter.
-s = np.random.multivariate_normal([0, 0], [[1,0],[0,1]], size=[5000])
+X = np.random.multivariate_normal([0, 0], [[1,0],[0,1]], size=[5000])
 plt.figure(figsize=(12,8))
-plt.scatter(s[:,0], s[:,1])
+plt.scatter(X[:,0], X[:,1])
 plt.xlabel('X1', fontsize=24)
 plt.ylabel('X2',fontsize=24)
 plt.title('Randomly sampled values from 2D Standard Normal Distribution',fontsize=24)
@@ -50,14 +46,35 @@ plt.show()
 # scatter plot of the transformed data points. Now check your answer by using the Python function
 # numpy.linalg.eig to obtain the trasformation and making a new scatter plot of the transformed
 # data points
-mu = np.array([2,6])
-Sigma = np.array([[2,1],[1,2]])
-Sigma2 = fractional_matrix_power(Sigma, 0.5)
-sy = np.dot(Sigma2, s.T) + numpy.matlib.repmat(mu, 5000, 1).T
-plt.scatter(sy.T[:,0], sy.T[:,1])
+A = np.array([[math.sqrt(3), 1], [math.sqrt(3), -1]]) / math.sqrt(2)
+b = np.array([[2, 6]] * 5000).T
+Y = (np.matmul(A, X.T) + b).T
+plt.figure(figsize=(12,8))
+plt.scatter(Y[:,0], Y[:,1])
 plt.xlabel('X1', fontsize=24)
 plt.ylabel('X2',fontsize=24)
 plt.title('Samples after affine transformation',fontsize=24)
 plt.show()
 
-# 
+Sigma = np.array([[2,1],[1,2]])
+w, v = np.linalg.eig(Sigma)
+w = w * np.eye(w.shape[0])
+A_np = np.matmul(v, np.sqrt(w))
+Y_np = (np.matmul(A_np, X.T) + b).T
+plt.figure(figsize=(12,8))
+plt.scatter(Y[:,0], Y[:,1])
+plt.xlabel('X1', fontsize=24)
+plt.ylabel('X2',fontsize=24)
+plt.title('Samples using np.linalg.eig',fontsize=24)
+plt.show()
+
+
+
+#
+
+
+
+# (c) (ii) Apply  the  affine  transformation  you  derived  in  part  (b)(iv)  
+# to  the  data  points,  and  make  ascatter plot of the transformed data points. 
+
+
